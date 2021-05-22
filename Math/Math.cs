677be673881f1f652c6b832204ef84beaf4d102b
OrlySedl <orlysedl@gmail.com>
2021-05-22 08:50:43 +0300

    Добавьте файлы проекта.

---
+++ Math/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Orsel
+{
+    public static class Math
+    {
+        public static int Factorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+    }
+}
