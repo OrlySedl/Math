41354c2a8eabeed7c7c07f4432f496d77c08a49d
OrlySedl <orlysedl@gmail.com>
2021-06-07 16:27:02 +0300

    Созданы тесты для AdjacencyMatrix.

--- TestMath/AdjacencyMatrixTest.cs
+++ TestMath/AdjacencyMatrixTest.cs
@@ -25,7 +25,33 @@ namespace TestMath
         }
 
         [TestMethod]
-        public void ToString_CreatingEmptyAdjacencyMatrix()
+        public void SetValue_GetValue_SetTrueInRow1Column0UndirectedGraph_true_true()
+        {
+            int vertices = 2;
+            int row = 1;
+            int col = 0;
+            AdjacencyMatrix am = new AdjacencyMatrix(vertices, TypeGraph.Undirected);
+            am.SetValue(row, col, true);
+
+            Assert.IsTrue(am.GetValue(row, col));
+            Assert.IsTrue(am.GetValue(col, row));
+        }
+
+        [TestMethod]
+        public void SetValue_GetValue_SetTrueInRow0Column1DirectedGraph_true_false()
+        {
+            int vertices = 2;
+            int row = 0;
+            int col = 1;
+            AdjacencyMatrix am = new AdjacencyMatrix(vertices, TypeGraph.Directed);
+            am.SetValue(row, col, true);
+
+            Assert.IsTrue(am.GetValue(row, col));
+            Assert.IsFalse(am.GetValue(col, row));
+        }
+
+        [TestMethod]
+        public void ToString_CreatingEmptyAdjacencyMatrix_string()
         {
             string expected = string.Empty;
             int vertices = 2;
@@ -38,6 +64,27 @@ namespace TestMath
                 expected += System.Environment.NewLine;
             }
             AdjacencyMatrix am = new AdjacencyMatrix(vertices, TypeGraph.Undirected);
+
+            Assert.AreEqual(expected, am.ToString());
+        }
+
+        [TestMethod]
+        public void ToString_CreatingFilledAdjacencyMatrix_string()
+        {
+            string expected = string.Empty;
+            int vertices = 2;
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                {
+                    expected += "1 ";
+                }
+                expected += System.Environment.NewLine;
+            }
+            AdjacencyMatrix am = new AdjacencyMatrix(vertices, TypeGraph.Undirected);
+            am.SetValue(0, 0, true);
+            am.SetValue(0, 1, true);
+            am.SetValue(1, 1, true);
 
             Assert.AreEqual(expected, am.ToString());
         }
