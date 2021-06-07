53056e38dd292435e0507c17e64ab4fb1e73ecbc
OrlySedl <orlysedl@gmail.com>
2021-06-07 15:51:52 +0300

    Созданы тесты для AdjacencyMatrix.

--- TestMath/AdjacencyMatrixTest.cs
+++ TestMath/AdjacencyMatrixTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Orsel.Math.Graph;
 
 namespace TestMath
 {
@@ -6,8 +7,39 @@ namespace TestMath
     public class AdjacencyMatrixTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Vertices_CreationWith4Vertices_4()
         {
+            int expected = 4;
+            AdjacencyMatrix am = new AdjacencyMatrix(expected, TypeGraph.Undirected);
+
+            Assert.AreEqual(expected, am.Vertices);
+        }
+
+        [TestMethod]
+        public void Type_CreationWithTypeGraphDirected_Directed()
+        {
+            TypeGraph expected = TypeGraph.Directed;
+            AdjacencyMatrix am = new AdjacencyMatrix(2, expected);
+
+            Assert.AreEqual(expected, am.Type);
+        }
+
+        [TestMethod]
+        public void ToString_CreatingEmptyAdjacencyMatrix()
+        {
+            string expected = string.Empty;
+            int vertices = 2;
+            for (int i = 0; i < vertices; i++)
+            {
+                for (int j = 0; j < vertices; j++)
+                {
+                    expected += "0 ";
+                }
+                expected += System.Environment.NewLine;
+            }
+            AdjacencyMatrix am = new AdjacencyMatrix(vertices, TypeGraph.Undirected);
+
+            Assert.AreEqual(expected, am.ToString());
         }
     }
 }
