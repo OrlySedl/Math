df0dde08f3b31c5246bde4e0c761fc02e3235fd2
OrlySedl <orlysedl@gmail.com>
2021-06-07 14:01:06 +0300

    Созданы: поля matrix и Vertices; Конструктор; Методы GetValue и SetValue.

--- Math/Graph/AdjacencyMatrix.cs
+++ Math/Graph/AdjacencyMatrix.cs
@@ -1,9 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 
 namespace Orsel.Math.Graph
 {
     class AdjacencyMatrix
     {
+        private BitArray[] matrix;
+
+        public readonly int Vertices;
+
+        public AdjacencyMatrix(int vertices)
+        {
+            Vertices = vertices;
+            matrix = new BitArray[vertices];
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = new BitArray(vertices);
+            }
+        }
+
+        public bool GetValue(int row, int col)
+        {
+            return matrix[row][col];
+        }
+
+        public void SetValue(int row, int col, bool value)
+        {
+            matrix[row][col] = value;
+        }
     }
 }
