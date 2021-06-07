d77b6a4af4dedff6be001fc3afcd3f5ea67ca601
OrlySedl <orlysedl@gmail.com>
2021-06-07 15:08:44 +0300

    Добавлен проект тестирования библиотеки Math. Создан файл теста для класса AdjacencyMatrix.

--- Math/Graph/AdjacencyMatrix.cs
+++ Math/Graph/AdjacencyMatrix.cs
@@ -3,15 +3,23 @@ using System.Collections;
 
 namespace Orsel.Math.Graph
 {
-    class AdjacencyMatrix
+    public enum TypeGraph
+    {
+        Undirected,
+        Directed,
+    }
+
+    public class AdjacencyMatrix
     {
         private BitArray[] matrix;
-
+        
         public readonly int Vertices;
+        public readonly TypeGraph Type;
 
-        public AdjacencyMatrix(int vertices)
+        public AdjacencyMatrix(int vertices, TypeGraph type)
         {
             Vertices = vertices;
+            Type = type;
             matrix = new BitArray[vertices];
             for (int i = 0; i < matrix.Length; i++)
             {
@@ -27,6 +35,31 @@ namespace Orsel.Math.Graph
         public void SetValue(int row, int col, bool value)
         {
             matrix[row][col] = value;
+            if (Type == TypeGraph.Undirected)
+            {
+                matrix[col][row] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = String.Empty;
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (matrix[row][col])
+                    {
+                        s += "1 ";
+                    }
+                    else
+                    {
+                        s += "0 ";
+                    }
+                }
+                s += Environment.NewLine;
+            }
+            return s;
         }
     }
 }
---
+++ TestMath/AdjacencyMatrixTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestMath
+{
+    [TestClass]
+    public class AdjacencyMatrixTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
