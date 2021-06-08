38529f3d3ca42e185e6b7d599707668c258c9e0c
OrlySedl <orlysedl@gmail.com>
2021-06-08 10:28:01 +0300

    Созданы: поля list, Vertices, Type; Конструктор.

--- Math/Graph/AdjacencyList.cs
+++ Math/Graph/AdjacencyList.cs
@@ -5,5 +5,20 @@ namespace Orsel.Math.Graph
 {
     class AdjacencyList
     {
+        private List<int>[] list;
+
+        public readonly int Vertices;
+        public readonly TypeGraph Type;
+
+        public AdjacencyList(int vertices, TypeGraph type)
+        {
+            Vertices = vertices;
+            Type = type;
+            list = new List<int>[Vertices];
+            for (int i = 0; i < list.Length; i++)
+            {
+                list[i] = new List<int>();
+            }
+        }
     }
 }
