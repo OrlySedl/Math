4a88253c179c11db36bb061f57bb89257894443a
OrlySedl <orlysedl@gmail.com>
2021-06-11 19:58:09 +0300

    Создан: абстрактный класс GraphStorage; конструктор; поля Vertices и Type; абстрактный метод AddEdge.

---
+++ Math/Graph/GraphStorage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orsel.Math.Graph
+{
+    public abstract class GraphStorage
+    {
+        public readonly int Vertices;
+        public readonly TypeGraph Type;
+
+        public GraphStorage(int vertices, TypeGraph type)
+        {
+            Vertices = vertices;
+            Type = type;
+        }
+
+        public abstract void AddEdge(int vertexBegin, int vertexEnd);
+    }
+}
